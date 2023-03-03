5da3a899f1517baff946baa3481cfba76ed0dbd1
italo <italogithub@gmail.com>
2023-03-03 10:46:42 -0300

    update

--- Learning/TestingClass.dart
+++ Learning/TestingClass.dart
@@ -1,24 +1,33 @@
+void main(){
+  Phone myPhone = Phone("Blue", 5, 3.800, 5.7);
+  Phone mySecondPhone = Phone("Red", 6, 0.500, 5.8);
+  double result = myPhone.phoneValue(1000);
+
+  print(myPhone.toString());
+  print("");
+  print(mySecondPhone.toString());
+  print("Valor: $result RS");
+ 
+}
+
+
 class Phone{
 
+  // utilizando o final eu não posso alterar o tipo da variável em nenhuma parte do código
   final String color;
   final int qtdProcess;
   final double tam;
   final double weight;
 
+
   Phone(this.color, this.qtdProcess, this.tam, this.weight);
 
   String toString(){
-    return "Cor: $color \nQuantidade de processadores: $qtdProcess \nPeso: $weight \nTamanho: $tam"; 
+    return "Cor: $color \nQuantidade de processadores: $qtdProcess \nPeso: $weight \nTamanho: $tam";  //concatenação no dart
   }
 
+  double phoneValue(double value){
+    return value * qtdProcess;
+  }
 }
 
-void main(){
-  Phone myPhone = Phone("Blue", 5, 0.800, 5.7);
-  Phone mySecondPhone = Phone("Red", 6, 0.500, 5.8);
-
-  print(myPhone.toString());
-  print("");
-  print(mySecondPhone.toString());
-  
-}
