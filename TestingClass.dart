ee04cb793519e88ecad2032165fa4e71a8db2668
italo <italogithub@gmail.com>
2023-03-03 09:32:21 -0300

    update

--- TestingClass.dart
+++ TestingClass.dart
@@ -1,8 +1,16 @@
 class Phone{
+
   final String color;
   final int qtdProcess;
   final double tam;
   final double weight;
 
   Phone(this.color, this.qtdProcess, this.tam, this.weight);
+}
+
+void main(){
+  Phone myPhone = Phone("Blue", 5, 0.800, 5.7);
+  Phone mySecondPhone = Phone("Red", 6, 0.500, 5.8);
+
+  print(myPhone.color);
 }
