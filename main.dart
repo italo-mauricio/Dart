38aea4e95b2ee3cae09afa8ea6fa119564fddd2b
italo <italogithub@gmail.com>
2023-03-03 05:59:44 -0300

    legalzinha hein

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
+import 'dart:mirrors';
+
 void main(List<String> args){
   print('testando dart');
+  String nome = "italo";
+  int idade = 23;
+  double peso = 80.9;
+
+  print(nome);
+  print(idade);
+  print(peso);
 }
