d913c01d08b27adf20dabb1608cef59467c255ea
italo <italo.santos.059@ufrn.edu.br>
2023-03-11 08:35:25 -0300

    update

--- Learning/LogicosOnDart.dart
+++ Learning/LogicosOnDart.dart
@@ -31,4 +31,25 @@ print("COMP: $testComp");
 bool testeOr = (true || false);
 print("OR: $testeOr");
 
+
+
+
+/**
+ *   Operador AND
+ * 
+ * true true -> true
+ * true false -> false
+ * false true -> false
+ * false false -> true
+ * 
+ */
+
+bool testeAnd = (true && true);
+print("ADN: $testeAnd");
+
+
+bool complex = (10 > 20) && ((30 < 20) || testeAnd);
+print(complex);
+
+
 }
