07cef1041272e744c1ac262d77c7ce36b5f98a0e
italo <italo.santos.059@ufrn.edu.br>
2023-03-11 08:31:59 -0300

    operador lógico

---
+++ Learning/LogicosOnDart.dart
@@ -0,0 +1,34 @@
+/* 
+    Comparadores
+
+  > maior
+  < menor
+  >= maior igual
+  <= menor igual
+  != diferente de
+  == igual
+
+*/
+void main(){
+
+
+double num1 = 15.0;
+bool testComp = (10 != num1);
+print("COMP: $testComp");
+
+
+
+/**
+ *   Operador OR
+ *
+ * true true -> true
+ * true false -> true
+ * false true -> true
+ * false false -> false
+ * 
+ * */
+
+bool testeOr = (true || false);
+print("OR: $testeOr");
+
+}
